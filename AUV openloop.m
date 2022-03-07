af0488c99ad7ef33f444bb6def81a4fef6a7da53
YoumnaMohamed <63843981+YoumnaMohamed@users.noreply.github.com>
2022-03-07 23:54:30 +0200

    Update AUV openloop.m

--- AUV openloop.m
+++ AUV openloop.m
@@ -1,6 +1,6 @@
 m=32.5; 
 g=9.81;
-v=0.03255; 
+V=0.03255; 
 p=1000;
 Zww=260;
 
@@ -12,10 +12,10 @@ pos  = zeros(1,(simTime/sampelTime)+1);
 vel  = zeros(1,(simTime/sampelTime)+1); 
 Fth  = zeros(1,(simTime/sampelTime));
 
-for i = 1:simTime/sampelTime
-    acceleration = (m*g - p*v*g - Zww*vel(i)*abs(vel(i)) - Fth(i))/m; 
-    vel(i+1)= vel(i) + sampTime*acceleration; 
-    pos(i+1) = pos(i) + sampTime*vel(i);
+for i = 1:sim_time/samp_time
+    acceleration = (m*g - p*V*g - Zww*vel(i)*abs(vel(i)) - Fth(i))/m;
+    vel(i+1) = vel(i) + samp_time*acceleration;
+    pos(i+1) = pos(i) + samp_time*vel(i);
 end
 
 plot(time,pos);
